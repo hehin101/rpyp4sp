34cd821e4549e43c8fe0e4e97bae7ec6ddcf49fa
CF Bolz-Tereick <cfbolz@gmx.de>
2025-09-01 13:49:36 +0200

    explicit test is now broken

--- rpyp4sp/test/test_fromjson.py
+++ rpyp4sp/test/test_fromjson.py
@@ -3,43 +3,6 @@ import os
 from rpyp4sp import p4specast, objects
 from rpyp4sp.rpyjson import loads
 
-decd_example = [
-      "DecD",
-      {
-        "it": "sum",
-        "note": None,
-        "at": {
-          "left": { "file": "spec/0-aux.watsup", "line": 18, "column": 5 },
-          "right": { "file": "spec/0-aux.watsup", "line": 18, "column": 9 }
-        }
-      },
-      [],
-      [],
-      []
-    ]
-
-def test_decd_example():
-    assert p4specast.DecD.fromjson(decd_example) == p4specast.DecD(
-        id=p4specast.Id(
-            value="sum",
-            region=p4specast.Region(
-                left=p4specast.Position(
-                    file="spec/0-aux.watsup",
-                    line=18,
-                    column=5
-                ),
-                right=p4specast.Position(
-                    file="spec/0-aux.watsup",
-                    line=18,
-                    column=9
-                )
-            )
-        ),
-        tparams=[],
-        args=[],
-        instrs=[]
-    )
-
 def test_full_ast_rpython():
     currfile = os.path.abspath(__file__)
     basedir = os.path.dirname(os.path.dirname(os.path.dirname(currfile)))
