e15f8ba4079393ad5f1383a2260c8fb78742da63
CF Bolz-Tereick <cfbolz@gmx.de>
2025-09-12 22:45:08 +0200

    introduce helper for testing

--- rpyp4sp/test/test_builtins.py
+++ rpyp4sp/test/test_builtins.py
@@ -1,25 +1,37 @@
 from rpyp4sp import p4specast, objects, builtin, context, integers
 
+def textlist(*args):
+    l = [objects.TextV(arg, typ=p4specast.TextT()) for arg in args]
+    return objects.ListV(l, typ=p4specast.IterT(p4specast.TextT(), p4specast.List()))
+
 def test_list_rev():
-    arg = objects.ListV([], -1, p4specast.IterT(p4specast.VarT(p4specast.Id('dir', p4specast.Region.line_span('spec/1a-syntax-el.watsup', 90, 7, 10)), []), p4specast.List()))
+    arg = textlist()
     res = builtin.lists_rev_(None, 'rev_', None, [arg])
     assert res is arg
 
-    arg = objects.ListV([objects.CaseV(p4specast.MixOp([[p4specast.AtomT('INOUT')]]), [], 13, p4specast.VarT(p4specast.Id('dir', p4specast.NO_REGION), [])), objects.CaseV(p4specast.MixOp([[p4specast.AtomT('NO')]]), [], 21, p4specast.VarT(p4specast.Id('dir', p4specast.NO_REGION), []))], -1, p4specast.IterT(p4specast.VarT(p4specast.Id('dir', p4specast.Region.line_span('spec/1a-syntax-el.watsup', 90, 7, 10)), []), p4specast.List()))
+    arg = textlist('a')
     res = builtin.lists_rev_(None, 'rev_', None, [arg])
-    exp = objects.ListV([objects.CaseV(p4specast.MixOp([[p4specast.AtomT('NO')]]), [], 21, p4specast.VarT(p4specast.Id('dir', p4specast.NO_REGION), [])), objects.CaseV(p4specast.MixOp([[p4specast.AtomT('INOUT')]]), [], 13, p4specast.VarT(p4specast.Id('dir', p4specast.NO_REGION), []))], -1, p4specast.IterT(p4specast.VarT(p4specast.Id('dir', p4specast.Region.line_span('spec/1a-syntax-el.watsup', 90, 7, 10)), []), p4specast.List()))
+    assert res is arg
+
+    arg = textlist('a', 'b')
+    res = builtin.lists_rev_(None, 'rev_', None, [arg])
+    exp = textlist('b', 'a')
     assert res.eq(exp)
 
 def test_list_distinct():
-    args = [objects.ListV([], -1, p4specast.IterT(p4specast.TextT(), p4specast.List()))]
+    args = [textlist()]
     res = builtin.lists_distinct_(None, 'distinct_', None, args)
     assert res.eq(objects.BoolV(True))
 
-    args = [objects.ListV([objects.TextV('Set_dmac', 14, p4specast.TextT()), objects.TextV('drop', 18, p4specast.TextT())], -1, p4specast.IterT(p4specast.TextT(), p4specast.List()))]
+    args = [textlist('a')]
     res = builtin.lists_distinct_(None, 'distinct_', None, args)
     assert res.eq(objects.BoolV(True))
 
-    args = [objects.ListV([objects.TextV('Set_dmac', 14, p4specast.TextT()), objects.TextV('Set_dmac', 18, p4specast.TextT())], -1, p4specast.IterT(p4specast.TextT(), p4specast.List()))]
+    args = [textlist('a', 'a')]
+    res = builtin.lists_distinct_(None, 'distinct_', None, args)
+    assert res.eq(objects.BoolV(False))
+
+    args = [textlist('a', 'b', 'a')]
     res = builtin.lists_distinct_(None, 'distinct_', None, args)
     assert res.eq(objects.BoolV(False))
 
