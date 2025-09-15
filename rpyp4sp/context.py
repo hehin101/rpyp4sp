e23a374e047130fdf2945980b0ad40c7208e98db
CF Bolz-Tereick <cfbolz@gmx.de>
2025-09-15 16:09:58 +0200

    refactor context a little bit

--- rpyp4sp/context.py
+++ rpyp4sp/context.py
@@ -34,11 +34,14 @@ def iterlist_to_key(l):
     return ''.join(key)
 
 class Context(object):
-    def __init__(self, filename, derive=False):
+    def __init__(self, filename, derive=False, glbl=None, local=None):
         self.filename = filename
-        self.glbl = GlobalContext()
-        self.local = LocalContext()
+        self.glbl = GlobalContext() if glbl is None else glbl
+        self.local = LocalContext() if local is None else local
         self.derive = derive
+
+    def copy_and_change_local(self, local):
+        return Context(self.filename, self.derive, self.glbl, local)
 
     def load_spec(self, spec):
         for definition in spec:
@@ -51,22 +54,13 @@ class Context(object):
                 self.glbl.fenv[definition.id.value] = definition
 
     def localize(self):
-        ctx = Context(self.filename, self.derive)
-        ctx.glbl = self.glbl
-        ctx.local = LocalContext()
-        return ctx
+        return self.copy_and_change_local(LocalContext())
 
     def localize_inputs(self, values_input):
-        ctx = Context(self.filename, self.derive)
-        ctx.glbl = self.glbl
-        ctx.local = ctx.local.copy_and_change(values_input=values_input)
-        return ctx
+        return self.copy_and_change_local(self.local.copy_and_change(values_input=values_input))
     
     def localize_venv(self, venv):
-        ctx = Context(self.filename, self.derive)
-        ctx.glbl = self.glbl
-        ctx.local = ctx.local.copy_and_change(venv=venv)
-        return ctx
+        return self.copy_and_change_local(self.local.copy_and_change(venv=venv))
 
     def find_value_local(self, id, iterlist):
         return self.local.venv[id.value, iterlist_to_key(iterlist)]
@@ -82,26 +76,19 @@ class Context(object):
         return self.glbl.renv[id.value]
 
     def add_value_local(self, id, iterlist, value):
-        result = Context(self.filename, self.derive)
-        result.glbl = self.glbl
-        result.local = LocalContext()
-        result.local.tdenv = self.local.tdenv
-        result.local.fenv = self.local.fenv
-        result.local.venv = self.local.venv.copy()
+        result = self.copy_and_change_local(self.local.copy_and_change(venv=self.local.venv.copy()))
         result.local.venv[id.value, iterlist_to_key(iterlist)] = value
         return result
 
     def add_func_local(self, id, func):
-        result = Context(self.filename, self.derive)
-        result.glbl = self.glbl
-        result.local = self.local.copy_and_change(fenv=self.local.fenv.copy())
+        result = self.copy_and_change_local(
+            self.local.copy_and_change(fenv=self.local.fenv.copy()))
         result.local.fenv[id.value] = func
         return result
 
     def add_typdef_local(self, id, typdef):
-        result = Context(self.filename, self.derive)
-        result.glbl = self.glbl
-        result.local = self.local.copy_and_change(tdenv=self.local.tdenv.copy())
+        result = self.copy_and_change_local(
+            self.local.copy_and_change(tdenv=self.local.tdenv.copy()))
         result.local.tdenv[id.value] = typdef
         return result
 
