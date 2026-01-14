6d5012bd1813c8b8852088bd6a4cfeac910a5878
Henrik Hinzmann <henrik.hinzmann@hhu.de>
2026-01-14 10:44:30 +0100

    fixed typ issue

--- rpyp4sp/objects.py
+++ rpyp4sp/objects.py
@@ -269,8 +269,8 @@ class NumVWithTyp(NumV):
     def get_typ(self):
         return self.typ
 
-NumV.NAT_ZERO = NumV.fromstr('0', p4specast.NatT.INSTANCE, p4specast.NatT.INSTANCE)
-NumV.NAT_ONE = NumV.fromstr('1', p4specast.NatT.INSTANCE, p4specast.NatT.INSTANCE)
+NumV.NAT_ZERO = NumV.fromstr('0', p4specast.NatT.INSTANCE, p4specast.NumT.NAT)
+NumV.NAT_ONE = NumV.fromstr('1', p4specast.NatT.INSTANCE, p4specast.NumT.NAT)
 
 class TextV(BaseVWithTyp):
     _compare_tag = 2
